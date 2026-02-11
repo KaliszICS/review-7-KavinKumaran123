aff1919118cbf26962e2b8d28012c03725ff5c7e
James Kalisz <james.kalisz@tdsb.on.ca>
2026-02-11 09:22:01 -0500

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -5,10 +5,11 @@ class Notes {
 		//if statement
 
 		int num;
-		num = 5;
+		num = 11;
 
 		int num3 = 0;//setup a default value for the variable
 		
+		//if (boolean or boolean expression) {code that runs when true}
 		if (num < 10) {
 			System.out.println(num + " was printed becuase if was true.");
 			num3 = 5;
