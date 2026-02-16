e82048e7154752fedafcd85c3b5288ea1c68cc81
KavinKumaran123 <166553090+KavinKumaran123@users.noreply.github.com>
2026-02-16 20:38:14 +0000

    Practice 7

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,78 @@
-public class PracticeProblem {
 
+public class PracticeProblem {
 	public static void main(String args[]) {
 
+		}
+	public static String evenOrOdd(int num){
+		if (num % 2 == 0){
+			return "Even";	
+		}
+		else{
+			return "Odd";
+		}
 	}
+	public static String teacherOrStudent(String name){
+		if (name.equals("Kalisz")){
+			return "Teacher";
+		}
+		else{
+			return "Student";
+		}
 
-	//Write your functions here
+	} 
+	public static int fartherFromZero(int a){
+		if (a <0){
+			return a-5;
+		}
+		else if (a>0){
+			return a+5;
+		
+		}
+		else{
+			return 0;
+		}
+	}
+	public static String isFive(int num){
+		if (num ==5){
+			return "The number is Five";
+		}
+		else{
+			return "The number is not Five";
+		}
+	}
+	public static String positiveOrNegative(double a){
+		if (a> 0){
+			return "Positive";
+		}
+		else{
+			return "Negative";
+		}
 
+	}
+	public static String highOrLow(int a){
+		if (a>100){
+			return "High";
+		
+		}
+		else{
+			return "Low";
+		}
+	}
+	public static String isHello(String a){
+		if (a.equals("Hello")){
+			return "The word is Hello";
+
+		}
+		else{
+			return "The word is not Hello";
+		}
+	}
 }
+	
+	
+
+
+
+	
+
+
